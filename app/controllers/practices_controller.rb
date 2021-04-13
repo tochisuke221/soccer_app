416166ef9694feb478a24b98a783782dcc8485cd
tochisuke221 <tochisuke221@gmail.com>
2021-04-13 17:19:24 +0900

    コントローラーとビューの作成

---
+++ app/controllers/practices_controller.rb
@@ -0,0 +1,4 @@
+class PracticesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'practices/new'
   root to: 'pages#home'
   devise_for :users
   resources :users,only: [:show,:edit,:update] 
