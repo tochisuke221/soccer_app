0cbc357575a0b81a2b370c27026119315a3dc988
tochisuke221 <tochisuke221@gmail.com>
2021-04-13 17:18:32 +0900

    バリデーションとActiveStorageへの紐付け

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,3 +1,16 @@
 class Practice < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :user_id
+    validates :title
+    validates :content
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :hardlevel_id
+  end
+
 end
