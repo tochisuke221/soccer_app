adbf093ad124c0b857bd1f91d0ad8ea7756fc988
tochisuke221 <tochisuke221@gmail.com>
2021-04-13 17:20:04 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'practices/new'
+  
   root to: 'pages#home'
   devise_for :users
   resources :users,only: [:show,:edit,:update] 
-
+  resources :practices,only: [:new,:create]
 
 end
