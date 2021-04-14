b0ae455a97914f3ce3a149fe3b699b4155d5906b
tochisuke221 <tochisuke221@gmail.com>
2021-04-14 17:10:18 +0900

    先に詳細ページへのリンク生成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'practices#index'
   devise_for :users
   resources :users,only: [:show,:edit,:update] 
-  resources :practices,only: [:new,:create]
+  resources :practices,only: [:new,:create,:show]
 
 end
