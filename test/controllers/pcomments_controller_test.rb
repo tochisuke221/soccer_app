4e8f8f5523f425cb7074bcd0f0e33c504c2fb3ba
tochisuke221 <tochisuke221@gmail.com>
2021-04-16 12:41:17 +0900

    コントローラの修正

--- test/controllers/pcomments_controller_test.rb
+++ test/controllers/pcomments_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CommentsControllerTest < ActionDispatch::IntegrationTest
+class PcommentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
