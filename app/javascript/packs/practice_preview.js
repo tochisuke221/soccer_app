2902d9959c79eba73373e3af456664edb0251f32
tochisuke221 <tochisuke221@gmail.com>
2021-04-18 22:14:06 +0900

    4枚以上の場合は投稿できない機能実装

--- app/javascript/packs/practice_preview.js
+++ app/javascript/packs/practice_preview.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log("ok");
   const ImageList = document.getElementById('practice-views');
+  const submit=document.getElementById("submit");
+  document.getElementById('practice_image').addEventListener('change', (e)=>{
+    while(ImageList.lastChild){
+      ImageList.removeChild(ImageList.lastChild);
+    }
+   });
   document.getElementById('practice_image').addEventListener('change', (e)=>{
     const files = e.target.files;
-    console.log(files);
+    if(files.length>4){
+      alert("投稿できる画像数は4枚までです");
+      submit.setAttribute("disabled",true);
+      return false;
+    }else{
+      submit.setAttribute("disabled",false);
+    }
     Array.from(files).forEach(file => {
       const blob = window.URL.createObjectURL(file);
       const imageElement = document.createElement('div');
@@ -13,12 +24,22 @@ document.addEventListener('DOMContentLoaded', function(){
        const blobImage = document.createElement('img');
        blobImage.setAttribute('src', blob);
        imageElement.appendChild(blobImage);
-       console.log(imageElement);
-       console.log(ImageList);
        ImageList.appendChild(imageElement);
     });
-    document.getElementById('practice_image').addEventListener('change', (e)=>{
-     imageElement.remove();
-    });
+    
+    // const blob = window.URL.createObjectURL(file);
+     // 画像を表示するためのdiv要素を生成
+    //  const imageElement = document.createElement('div');
+
+    //  // 表示する画像を生成
+    //  const blobImage = document.createElement('img');
+    //  blobImage.setAttribute('src', blob);
+    //  imageElement.appendChild(blobImage);
+    //  console.log(imageElement);
+    //  console.log(ImageList);
+    //  ImageList.appendChild(imageElement);
+    //  document.getElementById('practice_image').addEventListener('change', (e)=>{
+    //   imageElement.remove();
+    //  });
   });
 });
