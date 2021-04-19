76087b125d9df6f7aa5fb56a86ff215a604e8f01
tochisuke221 <tochisuke221@gmail.com>
2021-04-19 16:53:34 +0900

    モデルの作成（修正）

--- app/models/ptag.rb
+++ app/models/ptag.rb
@@ -1,2 +1,4 @@
 class Ptag < ApplicationRecord
+has_many :practice_tag_relations
+has_many ::practice
 end
--- db/migrate/20210419075322_create_practice_tag_relations.rb
+++ db/migrate/20210419075322_create_practice_tag_relations.rb
@@ -1,8 +1,7 @@
 class CreatePracticeTagRelations < ActiveRecord::Migration[6.0]
   def change
     create_table :practice_tag_relations do |t|
-      t.references :practice, foreign_key: true
-      t.references :ptag, foreign_key: true
+
       t.timestamps
     end
   end
