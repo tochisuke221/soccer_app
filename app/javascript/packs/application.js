9adcdf95ad1fed124b1f59a5fbb46aabb2714e09
tochisuke221 <tochisuke221@gmail.com>
2021-04-19 15:59:38 +0900

    レイアウトの調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import '../stylesheets/application';
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
--- app/javascript/packs/slider.js
+++ app/javascript/packs/slider.js
@@ -16,6 +16,7 @@
         infinite: true,
         speed: 300,
         slidesToShow: 1,
+       
         prevArrow: '<button class="slide-arrow prev-arrow"></button>',
         nextArrow: '<button class="slide-arrow next-arrow"></button>'
       });
