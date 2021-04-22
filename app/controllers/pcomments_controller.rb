44b150c172f070fcbd446aa6f705ebfb3cd15600
tochisuke221 <tochisuke221@gmail.com>
2021-04-22 22:10:12 +0900

    こっそり修正

--- app/controllers/pcomments_controller.rb
+++ app/controllers/pcomments_controller.rb
@@ -3,7 +3,7 @@ class PcommentsController < ApplicationController
     @practice=Practice.find(params[:practice_id])  
     @pcomment=Pcomment.new(pcomment_params)
     if @pcomment.save
-      @practice.create_notification_comment!(current_user,@pcomment.id) #投稿に紐づくコメントが来たという通知
+      @practice.create_notification_comment!(current_user,@pcomment.id,@practice.user_id) #投稿に紐づくコメントが来たという通知
       redirect_to practice_path(@practice)
     else
       @pcomments=Pcomment.where(practice_id:@practice.id)
--- app/models/practice.rb
+++ app/models/practice.rb
@@ -45,17 +45,8 @@ class Practice < ApplicationRecord
     notification.save if notification.valid?
   end
 
-  def create_notification_comment!(current_user,pcomment_id)
-    # ある投稿に対して、自分以外のコメント者をすべて取得し、全員に送る
-    temp_ids = Pcomment.select(:user_id).where(practice_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-        save_notification_comment!(current_user, pcomment_id, temp_id['user_id'])
-    end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, pcomment_id, user_id) if temp_ids.blank?
-  end
-
-  def save_notification_comment!(current_user, pcomment_id, visited_id)
+ 
+  def create_notification_comment!(current_user, pcomment_id, visited_id)
     #ある投稿に、あるコメントをしたユーザを登録する
     notification = current_user.active_notifications.new(
       practice_id: id,
