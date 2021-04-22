0e85748487b32b9217fe083fb06d1746c146fd84
tochisuke221 <tochisuke221@gmail.com>
2021-04-22 14:55:32 +0900

    notifiationのコントローラ設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,17 @@
 class NotificationsController < ApplicationController
   def index
+    #自分宛の通知を一気に集める
+    @notifications=current_user.passive_notifications
+    #未読・既読の更新を行う
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
   end
+
+  def destroy #全消去
+    @notifications = current_user.passive_notifications.destroy_all
+    redirect_to users_notifications_path
+  end
+
 end
+
