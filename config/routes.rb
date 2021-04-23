fdd68ff7b77a6b39b395b6f0c4fb40c87729cb1b
tochisuke221 <tochisuke221@gmail.com>
2021-04-23 15:37:37 +0900

    ゲストログイン機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   
   root to: 'practices#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
