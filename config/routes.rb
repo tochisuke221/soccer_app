b72e141585e27d57ef661ef9d410a9e3bfa109d0
tochisuke221 <tochisuke221@gmail.com>
2021-04-23 14:47:31 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   root to: 'practices#index'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :users,only: [:show,:edit,:update] do
     resources :relationships,only:[:create,:destroy]
   end
