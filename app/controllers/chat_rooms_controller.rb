ad0ca731857aece792d6fdb7ca9264a8183b18ae
tochisuke221 <tochisuke221@gmail.com>
2021-04-26 08:19:47 +0900

    コントローラー:indexアクションの設定

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,5 +1,12 @@
 class ChatRoomsController < ApplicationController
   
+  
+  
+  def index
+    current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room) #自身のチャットルーム全てを配列として集める
+    @chat_room_users=ChatRoomUser.where(chat_room: current_user_chat_rooms).where.not(user_id:current_user.id) #自身がが入るチャットルームにいる相手の中間テーブルを持ってくる
+  end
+  
   def create
     current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room) #自身のチャットルーム全てを配列として集める
     chat_room = ChatRoomUser.where(chat_room: current_user_chat_rooms, user_id: params[:user_id]).first
@@ -17,5 +24,7 @@ class ChatRoomsController < ApplicationController
     @chat_messages = ChatMessage.where(chat_room: @chat_room) #message内容を取得
     @chat_message=ChatMessage.new
   end
+
+
 
 end
