862f4b59683a286bf15fafc9203a247a150f6e2c
tochisuke221 <tochisuke221@gmail.com>
2021-04-26 09:26:50 +0900

    コントローラーの修正

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -4,20 +4,21 @@ class ChatRoomsController < ApplicationController
   
   def index
     current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room) #自身のチャットルーム全てを配列として集める
-    @chat_room_users=ChatRoomUser.where(chat_room: current_user_chat_rooms).where.not(user_id:current_user.id) #自身がが入るチャットルームにいる相手の中間テーブルを持ってくる
+    @chat_room_users=ChatRoomUser.includes(:user).where(chat_room: current_user_chat_rooms).where.not(user_id:current_user.id) #自身がが入るチャットルームにいる相手の中間テーブルを持ってくる
   end
   
   def create
     current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room) #自身のチャットルーム全てを配列として集める
-    chat_room = ChatRoomUser.where(chat_room: current_user_chat_rooms, user_id: params[:user_id]).first
+    chat_room = ChatRoomUser.where(chat_room: current_user_chat_rooms, user_id: params[:user_id]).map(&:chat_room).first
     if chat_room.blank?
       chat_room = ChatRoom.create #ルームの作成してから
       #中間テーブルに登録
       ChatRoomUser.create(chat_room: chat_room, user_id: current_user.id)
       ChatRoomUser.create(chat_room: chat_room, user_id: params[:user_id])
     end
-    redirect_to user_chat_room_path(params[:user_id],chat_room.chat_room_id)
+    redirect_to action: :show, id: chat_room.id
   end
+
   def show
     @chat_room = ChatRoom.find(params[:id]) #ルーム取得
     @chat_room_user = @chat_room.chat_room_users.where.not(user_id: current_user.id).first.user #ルーム相手の情報
