e89c0ce4ae44ceeef69f1da6d1d62076a185a5a0
tochisuke221 <tochisuke221@gmail.com>
2021-04-27 12:46:21 +0900

    ひたすらパンくず実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,86 @@
 crumb :root do
-  link "Home", root_path
+  link "投稿一覧", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :practice_new do
+  link "新規登録", new_user_registration_path
+  parent :root
+end
+crumb :login do
+  link "サインイン", new_user_session_path
+  parent :root
+end
+crumb :user_new do
+  link "新規投稿", new_practice_path
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :search do
+  link "検索結果"
+  parent :root
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :chat_room do
+  link "チャットルーム一覧"
+  parent :root
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
+crumb :rank do
+  link "いいねランキング"
+  parent :root
+end
 
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :notification do
+  link "通知一覧"
+  parent :root
+end
+
+crumb :practice_show do |practice|
+  link "投稿詳細", practice_path(practice)
+  parent :root
+end
+
+crumb :practice_edit do |practice|
+  link "投稿編集"
+  parent :practice_show,practice
+end
+
+
+
+
+
+crumb :user_show do |user|
+  link "#{user.name}さんの詳細", user_path(user)
+  parent :root
+end
+
+crumb :password do |user|
+  link "パスワード変更"
+  parent :user_show,user
+end
+
+crumb :user_edit do |user|
+  link "マイページ編集"
+  parent :user_show,user
+end
+
+crumb :chat do |chat_room_user|  
+  link "チャットルーム"
+  parent :user_show,chat_room_user
+end
+
+
+crumb :game_index do 
+  link "募集掲示板", games_path
+  parent :root
+end
+
+crumb :game_show do |game|
+  link "試合詳細", game_path(game)
+  parent :game_index
+end
+
+crumb :game_new do 
+  link "新規募集", new_game_path
+  parent :game_index
+end
