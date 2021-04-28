02b8f2763be57403a72db888ce6e644ac500ee2a
tochisuke221 <tochisuke221@gmail.com>
2021-04-28 09:00:58 +0900

    エラーメッセ対応

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,19 @@ class EventsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :your_schedule?
+  before_action :set_events,only:[:index,:create]
 
   def index
-    @events=Event.where(user_id:current_user.id).order(start_time: "ASC")
     @add_event=Event.new
   end
-
+  
   def create
-    Event.create(event_parameter)
-    redirect_to user_events_path(current_user)
+    @add_event=Event.new(event_params)
+    if @add_event.save
+      redirect_to user_events_path(current_user)
+    else 
+      render :index
+    end
   end
 
   def destroy
@@ -21,7 +25,7 @@ class EventsController < ApplicationController
 
   private
 
-  def event_parameter
+  def event_params
     params.require(:event).permit(:title, :content, :start_time).merge(user_id:current_user.id)
   end
 
@@ -29,6 +33,10 @@ class EventsController < ApplicationController
     unless current_user.id ==params[:user_id].to_i
       redirect_to user_path(current_user)
     end
+  end
+
+  def set_events
+    @events=Event.where(user_id:current_user.id).order(start_time: "ASC")
   end
 
 end
