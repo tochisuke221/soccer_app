88fcd4dd2f5d7a5424bf7e858b804cd63c318c88
tochisuke221 <tochisuke221@gmail.com>
2021-04-30 15:32:22 +0900

    Rspecの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+  end
+end
