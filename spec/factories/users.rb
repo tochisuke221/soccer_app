7d915b73a8bfe30343e0dbdbebfbbd5d34f426bd
tochisuke221 <tochisuke221@gmail.com>
2021-04-30 15:37:47 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name              {'test'}
+    name                  {'tochikawa'}
     email                 {'test@example'}
     password              {'000000'}
     password_confirmation {password}
     team_name             {'栃川高校'}
     career_id             {2}
     phone_num             {09012341234}
-
   end
 end
