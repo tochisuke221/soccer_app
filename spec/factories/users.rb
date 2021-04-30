b5e964aacd3670249a7357250166bd480f064c21
tochisuke221 <tochisuke221@gmail.com>
2021-04-30 15:44:42 +0900

    Fakerの導入と設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name                  {'tochikawa'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     team_name             {'栃川高校'}
-    career_id             {2}
-    phone_num             {09012341234}
+    career_id             {Faker::Number.within(range: 2..5)}
+    phone_num             {Faker::Number.number(digits: 12)}
   end
 end
