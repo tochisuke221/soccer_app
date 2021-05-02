72b3a78f9c0c2f2a924b5c5eb438296ec1a1818e
tochisuke221 <tochisuke221@gmail.com>
2021-05-02 13:07:04 +0900

    Eventモデルの単体テスト

---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    title               {Faker::Lorem.sentence}
+    start_time          {Faker::Time.between(from: DateTime.now + 1, to: DateTime.now)}
+    association :user
+  end
+end
