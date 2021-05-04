2b993958d8d15174ee51988f80ddf964bf8e3455
tochisuke221 <tochisuke221@gmail.com>
2021-05-04 18:39:17 +0900

    ActionCable heroku対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://rakukatsu-app.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://rakukatsu.herokuapp.com', 'http://rakukatsu.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
