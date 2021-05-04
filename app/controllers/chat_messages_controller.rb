f29bddc32972a170568ad2daa5a4838ccf799458
tochisuke221 <tochisuke221@gmail.com>
2021-05-04 23:49:37 +0900

    ミス修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -4,7 +4,7 @@ class ChatMessagesController < ApplicationController
   def create
     @chat_message = ChatMessage.new(chat_message_params)
     @another_user=ChatRoomUser.where(chat_room_id:@chat_message.chat_room_id).where.not(user_id:current_user)
-    ActionCable.server.broadcast 'chat_message_channel', content: @chat_message,another_id:@another_user[0].user_id if @chat_message.save
+    ActionCable.server.broadcast 'chat_message_channel', content: @chat_message,another_id:@another_user[0].user_id,current_user:current_user if @chat_message.save
     binding.pry
   end
 
