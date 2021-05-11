531fbf42cef9eea373a9414518d04aa3e9fe4cce
tochisuke221 <tochisuke221@gmail.com>
2021-05-11 21:46:03 +0900

    一部テスト成功パターン

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,18 @@
-require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, options: {
-      browser: :remote,
-      url: ENV.fetch("SELENIUM_DRIVER_URL"),
-      desired_capabilities: :chrome
-    }
-    Capybara.server_host = 'web'
-    Capybara.app_host='http://web'
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :selenium, using: :chrome, options: {
+        browser: :remote,
+        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+        desired_capabilities: :chrome
+      }
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 end
